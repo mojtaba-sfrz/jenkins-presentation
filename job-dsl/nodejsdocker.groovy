64e268fc1042afa696224cd37d09bf08d81fe942
mojtaba-sfrz <77304684+mojtaba-sfrz@users.noreply.github.com>
2023-06-06 06:16:08 +0330

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/mojtaba-sfrz/jenkins-presentation.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('msafarzayee@gmail.com')
         }
     }
     triggers {
